05d99b516d92ff8a290e9c4d2879d26d40b69a54
nateford23 <nathan.ford@hgs.hiddengeniusproject.org>
2017-07-10 11:47:54 -0700

    Initial Commit

---
+++ AuthenticationExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AuthenticationExample
+//
+//  Created by HGPMAC34 on 7/10/17.
+//  Copyright © 2017 HGPMAC34. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
